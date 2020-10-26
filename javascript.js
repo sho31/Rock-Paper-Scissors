a499acb435a3f8b8abaea109360f59b4889bf36a
Thomas <thomas.scholivet@gmail.com>
2020-10-26 22:42:35 +0100

    Ton of stuff

--- javascript.js
+++ javascript.js
@@ -49,6 +49,10 @@ test.textContent = "je suis lol"
 card.setAttribute("class", "card-body")
 card.innerHTML='<h4 class=\"card-title\" style=\"font-size: 42px;\">Score</h4>\n    <p class=\"card-text\" style=\"font-size: 37px;\"><span id="scoreMe" style=\"color: rgb(92,181,50);\">0</span><span> - </span><span id="scoreOpp"style=\"color: rgb(187,79,86);\">0</span></p>'
 const score = document.querySelector("#score")
+const br = document.querySelector("#rock")
+const bp = document.querySelector("paper")
+const bs = document.querySelector("scissors")
+
 function game() {
     let roundCount = 1;
     let wincount = 0;
@@ -58,10 +62,10 @@ function game() {
     const roundp = document.createElement("p");
     roundp.textContent = "Round " + roundCount
     card.appendChild(roundp)
-    while(roundCount <5) {
-            
+    const buttons = document.querySelectorAll('buttons');
+    alert(buttons);
+    buttons.forEach()
 
-    }
 }
 const startButton = document.querySelector("#startButton")
 startButton.addEventListener('click', game)
